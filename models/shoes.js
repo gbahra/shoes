acef5ccf0ea0841ed0df876e134980035e053e6e
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-02-15 22:23:22 +0000

    db populated models created and app.js started

---
+++ models/shoes.js
@@ -0,0 +1,17 @@
+var mongoose = reuqire('mongoose');
+
+var ShoeSchema = new mongoose.Schema({
+  name : {type: String, required:true},
+  colorway : {type: String, required:true},
+  material : {type: String, required:true},
+  price : {type: Number, required:true},
+  year: {type: Number, required:true}
+  //price is actually value
+  //check legit material
+
+  //maybe have brand for search purposes but for now
+  // put brand in the front of names
+});
+
+module.exports = mongoose.model('Shoe' , ShoeSchema);
+
