8140403c852f0223723cdc47f856b1c83cf8687d
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-02-18 09:48:25 +0000

    shoerack written but not fixed

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,15 @@
 var User= require("../models/user");
 var Shoe= require("../models/shoes");
-
+var currentUser;
 function newUser(req,res) {
-
-  res.render('users/new' , {title:"Register"});
-
+  res.render('users/new');
 }
 
 // CREATE - Handles registrations
 function createUser(req,res){
-
   // save the user
   var user = new User(req.body);
+  currentUser = user.id;
   user.save(function(err,user){
     // check for errors and return 500 if there was a problem
     if(err) req.flash('error' , err.message);
@@ -25,6 +23,10 @@ function updateUser(req,res){
     if(err) req.flash('error' , err.message);
     //find out whihc user and give them shoe.id on shoerack shoe_rack:shoe.id;
     //do this using first name on the screen somehow
+    User.findByIdAndUpdate(currentUser, {shoe_rack: shoe.id}function(err,user){
+      if(err) req.flash('error' , err.message);
+    })
+
 
     res.redirect("/");
   });
